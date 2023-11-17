494c60ac7ff73f3c3cf0a838f39352b5534b6322
dakaugu <dakaugu@gmail.com>
2023-11-16 22:00:27 -0800

    configuring ci/cd

---
+++ tests/test_yt_album.py
@@ -0,0 +1,20 @@
+import pytest
+
+from unittest.mock import Mock
+from finyl.yt_album import Album, DOWNLLOAD_PATH
+
+
+@pytest.fixture
+def mock_get(mocker):
+    mock = Mock()
+    mocker.patch("finyl.yt_album.Album.get", return_value=mock)
+    return mock
+
+
+def test_album_has_playlist_info(mock_get):
+    mock_get.return_value = None
+    album = Album("123")
+    assert album.id == "123"
+    assert album.playlist_path == f"{DOWNLLOAD_PATH}{album.id}"
+    assert album.playlist == None
+    assert isinstance(album.playlist, type(None))
