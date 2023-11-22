444bb342afad1b3606d15786e7f49d01560acb55
dakaugu <dakaugu@gmail.com>
2023-11-21 21:38:31 -0800

    some fixes

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ sudo apt install -y \
     libportaudiocpp0 \
     alsa-utils
 
+echo "Configure raspi-config"
+sudo raspi-config
+
 echo "Installing Poetry"
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="/home/ubuntu/.local/bin:$PATH"
@@ -49,7 +52,7 @@ echo "Add permission to file"
 sudo chmod 777 /var/
 
 echo "Install Python dependencies"
-poetry install
+poetry install --only main
 
 echo "Before running finyl, make sure to enable i2c in raspi config and disable HDMI audio"
 
