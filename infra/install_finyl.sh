febd8695d9bb227272bc39e33bacabaf5557e334
Dwight <dakaugu@gmail.com>
2023-11-27 23:27:40 -0500

    add infra to install finyl and run on boot up (#21)

---
+++ infra/install_finyl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Building finyl!"
+
+echo "Adding environment variables"
+export FINYL_ENV=FINYL_PI
+
+echo "installing necessary software"
+sudo apt update
+sudo apt install -y nala
+
+sudo nala install -y \
+    python3-pip \
+    i2c-tools \
+    libi2c-dev \
+    ffmpeg \
+    python3-pyaudio \
+    python3-dev \
+    libasound2-dev
+
+echo "Please configure raspi-config, and enable i2c and audio source"
+
+echo "Installing finyl!"
+pip install finyl-0.1.0.tar.gz --break-system-packages
+
+echo "Add permission to finyl files"
+sudo chmod 777 /var/
---
+++ infra/start_finyl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH="$HOME/.local/bin:$PATH"
+export FINYL_ENV=FINYL_PI
+
+finyl
