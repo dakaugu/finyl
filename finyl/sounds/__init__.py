4ea6f3f6c7a3c9465cfbd58de987efe4daff790a
dakaugu <dakaugu@gmail.com>
2023-12-18 22:52:01 -0800

    refactor sounds, added wifi connected sound

---
+++ finyl/sounds/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+BASE = os.path.dirname(__file__)
+FINYL_START = f"{BASE}/finyl_start.mp3"
+NFC_CONFIRMED = f"{BASE}/nfc_confirmed.wav"
+PING = f"{BASE}/ping.mp3"
+VINYL_SOUND = f"{BASE}/vinyl_crackle.mp3"
+WIFI_CONNECTED = f"{BASE}/wifi_connected.wav"
